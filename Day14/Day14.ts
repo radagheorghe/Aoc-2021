528193911ace11a6f01418edda08281f52c1b4a0
Gheorghe Rada <gheorghe.rada@caphyon.com>
2021-12-14 23:24:49 +0200

    Update Day14.ts

--- Day14/Day14.ts
+++ Day14/Day14.ts
@@ -17,48 +17,56 @@ class Polymerization {
 
   public apply(aSteps: number): number {
 
+    let occurs = new Map<string, number>();
     let connnections = new Map<string, number>();
 
-    const addConn = function(aConn: string, aFreq: number) {
-      let freq = connnections.get(aConn);
-      connnections.set(aConn, freq ? freq + aFreq : 1);
+    const addToMap = function(aMap: Map<string, number>, aKey: string, aValue: number) {
+      let value = aMap.get(aKey);
+      aMap.set(aKey, value == undefined ? aValue : value + aValue);
     }
 
-    for(let i = 0; i < this.mSeq.length - 1; i++)
-      addConn(this.mSeq[i] + this.mSeq[i+1], 1);
+    for(let i = 0; i < this.mSeq.length - 1; i++) {
+      addToMap(connnections, this.mSeq[i] + this.mSeq[i+1], 1);
+      addToMap(occurs, this.mSeq[i], 1);
+    }
+    addToMap(occurs, this.mSeq[this.mSeq.length - 1], 1);
 
-    for(let step = 0; step < aSteps -1; step ++) {
+    for(let step = 0; step < aSteps; step ++) {
 
-      console.log(step);
+      let conns = new Array<{pair: string, freq: number}>();
+      connnections.forEach((value, key) => {
+        conns.push({pair: key, freq: value})
+      })
+      
+      conns.forEach(conn => {
 
-      let conns = Array.from(connnections.keys());
-      let freqs = Array.from(connnections.values());
-
-      for(let i = 0; i < conns.length; i++) {
-
-        let conn = conns[i];
-        let freq = freqs[i];
-
-        addConn(conn[0] + this.mRules.get(conn), freq);
-        addConn(this.mRules.get(conn) + conn[1], freq);
+        let oldConnection = conn.pair;
+        let newOccure = this.mRules.get(oldConnection);
+        let newConnection1 = conn.pair[0] + newOccure;
+        let newConnection2 = newOccure + conn.pair[1];
         
-        let freq1 = connnections.get(conn);
-        if(freq1 != undefined && freq1 <= 1)
-          connnections.delete(conn);
-        else if(freq1 != undefined)
-          connnections.set(conn, freq1 - freq);
-      }      
+        addToMap(connnections, newConnection1, conn.freq);
+        addToMap(connnections, newConnection2, conn.freq);
+        addToMap(occurs, newOccure, conn.freq);
+
+        let freq = connnections.get(oldConnection);
+        if(freq - conn.freq < 1)
+          connnections.delete(oldConnection);
+        else
+          connnections.set(oldConnection, freq - conn.freq);
+      }) 
     }
 
-    console.log(connnections);
+    //console.log(occurs);
 
-    return 0;
+    let minMax = Array.from(occurs.values());
+    return Math.max(...minMax) - Math.min(...minMax);
   }
 }
 
 var input = new InputFile("./day14/input.txt");
 var input1 = new InputFile("./day14/input1.txt");
 
-var poly = new Polymerization(input1.getAsGroups(';'));
+var poly = new Polymerization(input.getAsGroups(';'));
 console.log(poly.apply(40));
 
